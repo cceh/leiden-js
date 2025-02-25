dcb7e3b951ec4c62446bb5c73e6634bcb0a4d64b
Marcel Schaeben <m.schaeben@uni-koeln.de>
2025-02-25 17:39:22 +0100

    wip

--- packages/dev-app/vite.config.js
+++ packages/dev-app/vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite'
 export default defineConfig(({ command }) => {
     return {
         base: command === 'build' ? '/leiden-js/' : '/',
+        build: {
+            sourcemap: true,
+        }
     }
 })
