3b84162c749f13308e1504cf68ab4850c06e544f
Marcel Schaeben <m.schaeben@uni-koeln.de>
2025-05-13 14:39:47 +0200

    dev-app: add trailing slash to env controlled base url if needed

--- packages/dev-app/vite.config.js
+++ packages/dev-app/vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import { env } from "node:process";
 
 export default defineConfig(({ command }) => {
+    const baseUrl = env.BASE_URL
+        ? (env.BASE_URL.endsWith("/") ? env.BASE_URL : `${env.BASE_URL}/`)
+        : (command === "build" ? "/leiden-js/" : "/");
+
     return {
-        base: env.BASE_URL || (command === "build" ? "/leiden-js/" : "/"),
+        base: baseUrl,
         build: {
             sourcemap: true,
         }
